1537e9d7496689e1f58910f0af4e0a563aef96d7
Vissg <vissg4444@gmail.com>
2024-11-15 13:47:10 +0700

    working application now!!!

--- Main.cpp
+++ Main.cpp
@@ -2,16 +2,25 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include <opencv2/core/utils/logger.hpp>
 
 #include <iostream>
 
 using namespace std;
 using namespace cv;
 
+const string ASCII_CHARS = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,^ `'. ";
+
+static string pixel_to_ascii(int intensity)
+{
+   string s = string(1, ASCII_CHARS[intensity * ASCII_CHARS.length() / 256]);
+   return s;
+}
+
 int main() 
 {
-
-   string image_path = samples::findFile("cat white.jpg");
+   cv::utils::logging::setLogLevel(cv::utils::logging::LogLevel::LOG_LEVEL_SILENT);
+   string image_path = samples::findFile("draven.jpg");
    Mat img = imread(image_path, IMREAD_COLOR);
 
    if ( img.empty())
@@ -31,9 +40,26 @@ int main()
    Mat res = img.clone();
    LUT(img, lookUpTable, res);
 
-   imshow("My image", img);
-   imshow("My gamma image", res);
+   int width = 235, height = 65;
+   Mat grey, resized;
+   string ascii;
+   cvtColor(img, grey, COLOR_BGR2GRAY);
+   resize(grey, resized, Size(width, height), 0, 0, INTER_AREA);
 
+   for ( int y = 0; y < height; y++ )
+   {
+      for ( int x = 0; x < width; x++ )
+      {
+         ascii += pixel_to_ascii(resized.at<uchar>(Point(x, y)));
+      }
+      ascii += "\n";
+   }
+
+   /*imshow("My image", img);
+   imshow("My grey image", grey);
+   imshow("My resized image", resized);*/
+   /*imshow("My gamma image", res);*/
+   cout << ascii;
    int k = waitKey(0); // Wait for a keystroke in the window
 
    return 0;
